0f77e2f1913419e178e07308d697dcdfd12d517a
Densovan <sarimramata@gmail.com>
2022-08-16 23:29:52 +0700

    react crud and form validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [formErrors, setFormErrors] = useState({});
   const [item, setItem] = useState([]);
   const [product, setProduct] = useState({
     id: "",
@@ -8,7 +9,7 @@ const App = () => {
     qty: "",
     price: "",
   });
-  const [formErrors, setFormErrors] = useState({});
+
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
@@ -33,6 +34,7 @@ const App = () => {
       setFormErrors({});
     }
   };
+
   const handleDelete = (param) => {
     const deleteItem = item.filter((item, index) => item.id !== param.id);
     setItem([...deleteItem]);
@@ -41,75 +43,69 @@ const App = () => {
 
   const validate = (values) => {
     const errors = {};
-    if (!values.name) {
-      errors.name = "Product name is require!";
-    }
     if (!values.id) {
-      errors.id = "Product id is require!";
+      errors.id = "Product id is required!";
+    }
+    if (!values.name) {
+      errors.name = "Product name is required!";
     }
     if (!values.qty) {
-      errors.qty = "Product qty is require!";
+      errors.qty = "Product qty is required!";
     }
     if (!values.price) {
-      errors.price = "Product price is require!";
+      errors.price = "Product price is required!";
     }
     return errors;
   };
-  // useEffect(() => {
-  //   if (Object.keys(formErrors).length === 0 && isSubmit) {
-  //     console.log("prodcut", product);
-  //   }
-  //   console.log(formErrors);
-  // }, [formErrors]);
+
   return (
     <div className="grid grid-cols-2 mt-56">
       <div className="flex justify-center items-center">
         <div className="bg-white w-2/5 p-6 rounded">
-          <h1 className="text-center font-bold">ADD PRODUCT</h1>
-          <br />
+          <h1 className="uppercase text-2xl text-center">add product</h1>
           <form onSubmit={handleSubmit}>
-            <label className="block">ID</label>
+            <lable className="block">ID</lable>
             <input
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              onChange={handleChange}
               name="id"
               value={product.id}
-              placeholder="input product id"
+              onChange={handleChange}
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Input product ID"
             />
             <p className="text-red-900">{formErrors.id}</p>
             <br />
-            <label className="block">Name</label>
+            <lable className="block">Product Name</lable>
             <input
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              onChange={handleChange}
               name="name"
               value={product.name}
-              placeholder="input product name"
+              onChange={handleChange}
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Input product Name"
             />
             <p className="text-red-900">{formErrors.name}</p>
             <br />
-            <label className="block">QTY</label>
+            <lable className="block">QTY</lable>
             <input
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              onChange={handleChange}
               name="qty"
               value={product.qty}
-              placeholder="input product qty"
+              onChange={handleChange}
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Input product QTY"
             />
             <p className="text-red-900">{formErrors.qty}</p>
             <br />
-            <label className="block">price</label>
+            <lable className="block">Price</lable>
             <input
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              onChange={handleChange}
               name="price"
               value={product.price}
-              placeholder="input product price"
+              onChange={handleChange}
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Input product Price"
             />
             <p className="text-red-900">{formErrors.price}</p>
             <br />
-            <button className="bg-gray-900 text-white font-semibold uppercase px-11 py-2 rounded-xl justify-center">
-              submit
+            <button className="px-10 py-2 text-md text-white bg-gray-700 rounded">
+              Submit
             </button>
           </form>
         </div>
@@ -118,29 +114,29 @@ const App = () => {
         <div className="grid grid-cols-3 gap-4">
           {item.map((res, index) => {
             return (
-              <div className="bg-white p-5 " key={index}>
+              <div className="bg-white p-5 rounded-xl" key={index}>
                 <button
-                  className="float-right bg-red-500 px-2 text-white rounded-full"
                   onClick={() => handleDelete(res)}
+                  className="float-right bg-red-500 px-2 text-white rounded-full"
                 >
                   x
                 </button>
                 <div className="mt-6 text-center">
                   <div className="flex space-x-4 items-center">
-                    <p>Product ID :</p>
-                    <h1 className="text-xl">{res.id}</h1>
+                    <h1>Product ID:</h1>
+                    <p className="text-xl">{res.id}</p>
                   </div>
                   <div className="flex space-x-4 items-center">
-                    <p>Product Name :</p>
-                    <h1 className="text-xl">{res.name}</h1>
+                    <h1>Product Name:</h1>
+                    <p className="text-xl">{res.name}</p>
                   </div>
                   <div className="flex space-x-4 items-center">
-                    <p>Product Price :</p>
-                    <h1 className="text-xl">${res.price}</h1>
+                    <h1>Product QTY:</h1>
+                    <p className="text-xl">{res.qty}</p>
                   </div>
                   <div className="flex space-x-4 items-center">
-                    <p>Product Quantity :</p>
-                    <h1 className="text-xl">{res.qty}</h1>
+                    <h1>Product Price:</h1>
+                    <p className="text-xl">{res.price}</p>
                   </div>
                 </div>
               </div>
